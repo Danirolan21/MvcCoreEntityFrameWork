d6286f698f147583e7947e646509c46029a4eb5d
Danirolan21 <danirolan21@hotmail.com>
2025-02-12 12:36:59 +0100

    Add project files.

---
+++ MvcCoreEF/Controllers/HospitalesController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreEF.Repositories;
+using MvcCoreEF.Models;
+using Microsoft.Extensions.Hosting;
+
+namespace MvcCoreEF.Controllers
+{
+    public class HospitalesController : Controller
+    {
+        RepositoryHospital repo;
+
+        public HospitalesController(RepositoryHospital repo)
+        {
+            this.repo = repo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Hospital> hospitales = 
+                await this.repo.GetHospitalesAsync();
+            return View(hospitales);
+        }
+
+        public async Task<IActionResult> Details(int idHospital)
+        {
+            Hospital hosp =
+                await this.repo.FindHospitalAsync(idHospital);
+            return View(hosp);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Hospital hosp)
+        {
+            await this.repo.InsertHospitalAsync(hosp.IdHospital
+                , hosp.Nombre, hosp.Direccion, hosp.Telefono, hosp.Camas);
+            //REDIRECT CON PARAMETROS
+            return RedirectToAction("Details", new { idHospital = hosp.IdHospital });
+        }
+
+        public async Task<IActionResult> Delete(int idHospital)
+        {
+            await this.repo.DeleteHospitalAsync(idHospital);
+            return RedirectToAction("Index");
+
+        }
+
+        public async Task<IActionResult> Update(int idHospital)
+        {
+            Hospital hosp =
+                await this.repo.FindHospitalAsync(idHospital);
+            return View(hosp);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Hospital hosp)
+        {
+            await this.repo.UpdateHospitalAsync(hosp.IdHospital
+                , hosp.Nombre, hosp.Direccion, hosp.Telefono, hosp.Camas);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcCoreEF/Repositories/RepositoryHospital.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreEF.Data;
+using MvcCoreEF.Models;
+
+namespace MvcCoreEF.Repositories
+{
+    public class RepositoryHospital
+    {
+        private HospitalContext context;
+
+        public RepositoryHospital(HospitalContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Hospital>> GetHospitalesAsync()
+        {
+            var consulta = from datos in this.context.Hospitales
+                           select datos;
+            return await consulta.ToListAsync();
+        }
+
+        public async Task<Hospital> FindHospitalAsync(int idHospital)
+        {
+            var consulta = from datos in this.context.Hospitales
+                           where datos.IdHospital == idHospital
+                           select datos;
+            return await consulta.FirstOrDefaultAsync();
+        }
+
+        public async Task
+            InsertHospitalAsync(int idHospital, string nombre
+            , string direccion, string telefono, int camas)
+        {
+            //CREAMOS UN MODEL
+            Hospital hosp = new Hospital();
+            //ASIGNAMOS SUS PROPIEDADES
+            hosp.IdHospital = idHospital;
+            hosp.Nombre = nombre;
+            hosp.Direccion = direccion;
+            hosp.Telefono = telefono;
+            hosp.Camas = camas;
+            //AÑADIMOS NUESTRO MODEL A LA COLECCION DBSET DEL CONTEXT
+            await this.context.Hospitales.AddAsync(hosp);
+            //INDICAMOS QUE ALMACENE LOS DATOS EN LA BBDD
+            await this.context.SaveChangesAsync();
+        }
+
+        public async Task DeleteHospitalAsync(int idHospital)
+        {
+            Hospital hospital =
+                await this.FindHospitalAsync(idHospital);
+            this.context.Hospitales.Remove(hospital);
+            await this.context.SaveChangesAsync();
+        }
+
+        public async Task
+            UpdateHospitalAsync(int idHospital, string nombre
+            , string direccion, string telefono, int camas)
+        {
+            //BUSCAMOS EL OBJETO HOSPITAL A MODIFICAR
+            Hospital hosp =
+                await this.FindHospitalAsync(idHospital);
+            //PODEMOS MODIFICAR TODO LO QUE DESEEMOS EXCEPTO
+            //EL CAMPO [Key]
+            hosp.Nombre = nombre;
+            hosp.Direccion = direccion;
+            hosp.Telefono = telefono;
+            hosp.Camas = camas;
+            //NO TENEMOS NINGUN METODO PARA REALIZAR UN UPDATE
+            //DENTRO DEL CONTEXT Y DbSet<Y>
+            await this.context.SaveChangesAsync();
+        }
+    }
+}
